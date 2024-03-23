2292b52f044339ddfaee3e66a540ff65c305814a
Pavel <145974996+ascezis@users.noreply.github.com>
2024-03-23 17:48:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+import markersTrack from "/data.js";
+
+// Запуск карты
+ymaps.ready(async () => {
+
+    let map_init = new ymaps.Map('map', {
+        center: [55.333052, 100.732638], zoom: 15
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+    // Перебор объектов
+    await markersTrack.forEach(data => {
+
+        let marker = new ymaps.Placemark(data.geometry.coordinates, {
+            iconCaption: data.properties.iconCaption,
+
+            // Кастомное содержимое:
+            balloonContentBody: `
+            <div>
+                <p style="text-align: center; font-family: Consolas">${data.properties.iconCaption}</p>
+                <img src=${data.properties.imageSrc} alt="current" style="width: 100%; border-radius: 1%">
+            </div>`
+
+        }, {
+            preset: 'islands#blueCircleDotIconWithCaption' // Устанавливаем тип метки как точка
+
+        });
+        let button_press = document.getElementById('button_press');
+        map_init.geoObjects.add(marker)
+    });
+});
